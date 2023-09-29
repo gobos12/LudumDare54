c3333462c19ca4f5655d1a79dff36a5d0e578311
Orion Dobos <oriondobos@gmail.com>
2023-09-29 16:49:20 -0700

    post processing & other scripts

---
+++ Assets/Scripts/FridgeMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FridgeMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject leftDoor;
+    [SerializeField] private GameObject rightDoor;
+    private bool isOpen;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OpenDoor()
+    {
+        
+        
+    }
+    
+}
