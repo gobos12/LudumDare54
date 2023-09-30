210737657c120b38c83e26e52277ed0b96906082
Orion Dobos <oriondobos@gmail.com>
2023-09-29 17:45:01 -0700

    doors be opening

--- Assets/Scripts/FridgeMovement.cs
+++ Assets/Scripts/FridgeMovement.cs
@@ -6,7 +6,10 @@ public class FridgeMovement : MonoBehaviour
 {
     [SerializeField] private GameObject leftDoor;
     [SerializeField] private GameObject rightDoor;
-    private bool isOpen;
+
+    public Camera camera;
+    private Ray ray;
+    private RaycastHit hit;
     void Start()
     {
         
@@ -15,12 +18,38 @@ public class FridgeMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Hover();
     }
+
+    private void Hover()
+    {
+        ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray.origin, ray.direction, out hit))
+        {
+            Debug.Log(hit.collider.name);
+            OpenDoor();
+        }
+
+
+    }
+    
 
     private void OpenDoor()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (hit.collider.name.Contains("Door"))
+            {
+                hit.collider.gameObject.transform.rotation = hit.collider.gameObject.GetComponent<ObjectState>().isOpen ? new Quaternion(0, 0, 0, 0) : new Quaternion(0, 90, 0, 0);
+                hit.collider.gameObject.GetComponent<ObjectState>().isOpen =
+                    !hit.collider.gameObject.GetComponent<ObjectState>().isOpen;
+            }
+            else //Drawers
+            {
+                
+            }
+        }
         
     }
     
